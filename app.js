5cee045626df25678c1d3f3f271ad16c6b94446f
chao zhao <cszhaochao@gmail.com>
2022-04-01 14:11:22 -0400

    this is the start of the project

---
+++ app.js
@@ -0,0 +1,19 @@
+// jshint esversion:6
+
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is up and running!");
+});
